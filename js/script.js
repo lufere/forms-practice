5e3928d562593fec4bdec5d0ecb84ff09e2a2276
Luis Rendon <luisfelipe.rendon@gmail.com>
2020-06-08 23:06:15 -0700

    Added zip code validation

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 let email = document.querySelector('#email');
 
+let zipcode = document.querySelector('#zip');
+
 
 email.addEventListener('focusout', function(){
     if (email.validity.typeMismatch){
@@ -7,5 +9,14 @@ email.addEventListener('focusout', function(){
     }
     if (email.validity.tooShort){
         alert("Email must be at least 7 characters long!");
+    }
+})
+
+zipcode.addEventListener('focusout', function(){
+    if(!zipcode.validity.valid && (!zipcode.validity.rangeUnderflow && !zipcode.validity.rangeOverflow)){
+        alert("Zip codes are just numbers!");
+    }
+    if (zipcode.validity.rangeUnderflow || zipcode.validity.rangeOverflow){
+        alert("Zip codes are 5 characters long!");
     }
 })
